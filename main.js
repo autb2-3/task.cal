973f88331945e90926906b4d689e765a85da7c9c
EC2 Default User <ec2-user@ip-172-31-30-229.ec2.internal>
2021-08-25 15:56:07 +0000

    dentaku

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
 let display = document.getElementById("display");
 
 
-if(display.value===null){
+if(display.value==""){
   display.value="0";
 }
 
-
 function edit(elem) {
-  display.value=display.value+elem.value ;
+  let start = display.value.slice(0,1);
+  
+  if(start=="0"){
+    
+    display.value=null;
+    display.value=display.value+elem.value ;
+  }
+  else{
+    display.value=display.value+elem.value ;
+  }
   // body...
 }
 
+function ope(elem){
+  let last = display.value.slice(-1);
+  if(last=="+" || last=="-" ||last=="*"||last=="/"){
+    display.value=display.value.slice(0,-1);
+    display.value=display.value+elem.value ;
+  }else{
+    display.value=display.value+elem.value ;
+  }
+  
+}
 function AC(){
   display.value=null;
 }
